853486c0d8631f0b4e9e5a4cf4809d3225c530dc
Idan Kapustian <idan.kapustian@gmail.com>
2025-02-01 20:08:18 +0200

    Fixed the report descriptor to include the padding between mouse click and movement

--- app/src/main/java/com/example/handymouse/HidService.kt
+++ app/src/main/java/com/example/handymouse/HidService.kt
@@ -29,7 +29,10 @@ class HidService(base: Context?) : ContextWrapper(base) {
         0x25, 0x01,     //     Logical Maximum (1)
         0x95, 0x03,     //     Report Count (3)
         0x75, 0x01,     //     Report Size (1)
-        0x81, 0x02,     //     Input (Data, Variable, Absolute)
+        0x81, 0x02,     //     Input (Data, Variable, Absolute) ; 3 bit (Left, Right, Middle)
+        0x95, 0x01,     //     Report Count (1)
+        0x75, 0x05,     //     Report Size (5)
+        0x81, 0x01,     //     Input (Constant) ; 5 bit padding
         0x05, 0x01,     //     Usage Page (Generic Desktop)
         0x09, 0x30,     //     Usage (X)
         0x09, 0x31,     //     Usage (Y)
@@ -37,7 +40,7 @@ class HidService(base: Context?) : ContextWrapper(base) {
         0x25, 0x7F,     //     Logical Maximum (127)
         0x75, 0x08,     //     Report Size (8)
         0x95, 0x02,     //     Report Count (2)
-        0x81, 0x06,     //     Input (Data, Variable, Relative)
+        0x81, 0x06,     //     Input (Data, Variable, Relative) ; 2 bytes (X, Y)
         0xC0,           //   End Collection
         0xC0,           // End Collection
     ).map { it.toByte() }.toByteArray()
